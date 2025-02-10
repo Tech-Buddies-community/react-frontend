0358dc98a15dcde8d5b9caefa172f9024c8aec4d
Lutfi Cahya Nugraha <halolutfii@gmail.com>
2025-02-11 00:35:53 +0700

    feat: fix format date

--- src/Utils/Index.jsx
+++ src/Utils/Index.jsx
@@ -1,17 +1,14 @@
 export const formatDate = (dateString) => {
-    if (!dateString || !/\d{2}\/\d{2}\/\d{4}/.test(dateString)) return "Invalid Date";
-
-    const [day, month, year] = dateString.split('/').map(Number); // Convert ke angka
-    const dateObj = new Date(year, month - 1, day); // Gunakan cara yang benar
+    const [day, month, year] = dateString.split('/'); // Pecah format dd/mm/yyyy
+    const dateObj = new Date(`2025-01-01T${year}-${month}-${day}`); // Buat Date object
 
     return dateObj.toLocaleDateString('en-US', {
-        weekday: 'long',  // Nama hari
+        weekday: 'long',  // Nama hari (Senin, Selasa, ...)
         day: '2-digit',    // Tanggal
         month: 'long',     // Nama bulan
         year: 'numeric'    // Tahun
     });
-};
-
+}
 
 export const formatTime = (timeString) => {
     if (!timeString) return ""; // Cegah error jika null atau undefined
