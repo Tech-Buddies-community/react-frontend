98c55ee10eccdcac33cb8957c8ea258e940f2019
Lutfi Cahya Nugraha <halolutfii@gmail.com>
2025-02-11 11:16:00 +0700

    feat: add pagination in program

--- src/pages/ProgramView.jsx
+++ src/pages/ProgramView.jsx
@@ -4,25 +4,25 @@ import { Link, useLoaderData } from "react-router-dom";
 import { useSelector } from "react-redux";
 import FilterProgram from "../components/FilterProgram";
 import CardProgram from "../components/CardProgram";
+import Pagination from "../components/Pagination";
 
 export const loader = async ({request}) => {
     const params = Object.fromEntries([
         ...new URL(request.url).searchParams.entries(),
     ]);
 
-
     const { data } = await customAPI.get('/program', {params: params});
 
     // console.log(params);
     const program = data.data;
     // console.log(program);
-    // const pagination = data.pagination;
+    const pagination = data.pagination;
 
-    return { program, params };
+    return { program, params, pagination };
 }
 
 const ProgramView = () => {
-    const { program } = useLoaderData();
+    const { program, pagination } = useLoaderData();
     const user = useSelector((state) => state.userState.user);
 
     return (
@@ -33,19 +33,21 @@ const ProgramView = () => {
                 <h2 className="text-2xl font-bold capitalize">Program List</h2>
             </div>
 
+            <h3 className="text-2xl text-neutral font-bold text-right my-3">Total : {pagination.totalProgram} Program</h3>
+
             {user && user.role === 'admin' && (
                 <div className="mt-2 flex justify-end">
                         <Link to='/program/create' className="btn btn-neutral">Add Program</Link>
                 </div>
             )}
 
-        {!program.length ? ( 
-                <div className="mt-5 mb-5">
-                    <h1 className="text-3xl text-center">
-                        Oops! Kami tidak dapat menemukan program yang sesuai dengan pencarianmu. Mungkin coba kata kunci lain? 🤔
-                    </h1>
-                </div>
-            ) : (  
+            {!program.length ? ( 
+                    <div className="mt-5 mb-5">
+                        <h1 className="text-3xl text-center">
+                            Oops! Kami tidak dapat menemukan program yang sesuai dengan pencarianmu. Mungkin coba kata kunci lain? 🤔
+                        </h1>
+                    </div>
+                ) : (  
                 <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-5 mt-5">
                     {program.map((item) => (
                         <CardProgram item={item} key={item._id} user={user} />
@@ -53,6 +55,10 @@ const ProgramView = () => {
                     }
                 </div>
             )}
+
+            <div className="mt-5 flex justify-center">
+                <Pagination />
+            </div>
         </>
     )
 }
