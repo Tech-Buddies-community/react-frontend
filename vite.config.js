77e8bf1ce73b165a4bedff64c9cd4f0572701d6c
HikarusV <nazwapradana.pc@gmail.com>
2025-02-16 13:21:01 +0700

    [setup]: auto deploymen in branch dev

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import dotenv from "dotenv";
 import react from '@vitejs/plugin-react'
+
+dotenv.config();
+
+const API_URL = process.env.VITE_API_URL || 'http://localhost:3000'
+const APP_PORT = process.env.VITE_APP_PORT || 5176
+
+console.log(`APP RUN ON PORT '${APP_PORT}'`);
+console.log(`LISTEN ON BACKEND URL '${API_URL}'`);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,12 +16,11 @@ export default defineConfig({
   server: {
       proxy: {
         '/api': {
-          target: 'https://techbuddies-be.vercel.app',
-          // target: 'http://localhost:3000',
+          target: API_URL,
           changeOrigin: true,
         },
-
       },
+      port: APP_PORT,
     }
   },
 )
