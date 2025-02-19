05aab73334534b7b83087fdd6b2ffc58e103e461
HikarusV <nazwapradana.pc@gmail.com>
2025-02-19 21:10:57 +0700

    [fix]: remove vite proxy rewrites

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,6 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: process.env.VITE_ENV == "dev",
-      },
-    },
     port: Number(process.env.VITE_APP_PORT) || 5173,
   }
 });
