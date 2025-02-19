65e871ab6de149259fb96f5730ba0540fbffc81e
Lutfi Cahya Nugraha <halolutfii@gmail.com>
2025-02-19 13:56:00 +0700

    fix: fixing page white blank

--- src/features/userSlice.jsx
+++ src/features/userSlice.jsx
@@ -1,8 +1,18 @@
-import { createSlice } from '@reduxjs/toolkit' ;
+import { createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
 const initialState = {
-    user: JSON.parse(localStorage.getItem('user')) || null
+    user: null
+};
+
+try {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+        initialState.user = JSON.parse(userData);
+    }
+} catch (error) {
+    console.error('Error parsing user data from localStorage:', error);
+    localStorage.removeItem('user'); // Hapus data yang rusak
 }
 
 const userSlice = createSlice({
@@ -10,27 +20,22 @@ const userSlice = createSlice({
     initialState,
     reducers: {
         loginUser: (state, action) => {
-            const user = action.payload.data
-
-            // Set nilai dari state
-            state.user = user 
-
-            // Set localstorage
-            localStorage.setItem('user', JSON.stringify(user))
+            const user = action.payload.data;
+            state.user = user;
+            localStorage.setItem('user', JSON.stringify(user));
         },
         logoutUser: (state) => {
-            state.user = null
-            localStorage.removeItem('user')
-            toast.success('Logout Success!')
+            state.user = null;
+            localStorage.removeItem('user');
+            toast.success('Logout Success!');
         },
         registerUser: (state, action) => {
-            const user = action.payload.data
-            state.user = user
-            // Set localstorage
-            localStorage.setItem('user', JSON.stringify(user))
+            const user = action.payload.data;
+            state.user = user;
+            localStorage.setItem('user', JSON.stringify(user));
         }
     }
-})
+});
 
 export const { loginUser, logoutUser, registerUser } = userSlice.actions;
-export default userSlice.reducer
+export default userSlice.reducer;
