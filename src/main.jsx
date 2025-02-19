41d5587e49cf2a8701055d8b31be43210da5b087
HikarusV <nazwapradana.pc@gmail.com>
2025-02-19 20:33:35 +0700

    [debug]: env data

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
+console.log(`APP RUN ON PORT '${import.meta.env.VITE_APP_PORT}'`);
+console.log(`LISTEN ON BACKEND URL '${import.meta.env.VITE_API_URL}'`);
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
       <App />
