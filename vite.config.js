ed7ee2145b09dcdbde1d47706fc92ca5a0fb043c
HikarusV <nazwapradana.pc@gmail.com>
2025-02-19 20:33:22 +0700

    [fix]: remove console.log and move read env

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,16 @@ import react from '@vitejs/plugin-react'
 
 dotenv.config();
 
-const API_URL = process.env.VITE_API_URL || 'http://localhost:3000'
-const APP_PORT = process.env.VITE_APP_PORT || 5173
-
-console.log(`APP RUN ON PORT '${APP_PORT}'`);
-console.log(`LISTEN ON BACKEND URL '${API_URL}'`);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-      proxy: {
-        '/api': {
-          target: API_URL,
-          changeOrigin: true,
-        },
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
       },
-      port: APP_PORT,
-    }
-  },
-)
+    },
+    port: Number(process.env.VITE_APP_PORT) || 5173,
+  }
+});
