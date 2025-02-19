5c4b74199bb7ef9b6276cdc1cea508763865a1c7
HikarusV <nazwapradana.pc@gmail.com>
2025-02-19 20:46:26 +0700

    [fix]: prod rewrite url

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:3000',
-        changeOrigin: true,
+        secure: false,
+        changeOrigin: process.env.VITE_ENV == "dev",
       },
     },
     port: Number(process.env.VITE_APP_PORT) || 5173,
