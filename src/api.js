fd3dcd43567534712219da055f43b223cea70e1d
HikarusV <nazwapradana.pc@gmail.com>
2025-02-19 21:11:14 +0700

    [fix]: api base url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_URL = import.meta.env.VITE_API_URL
 const customAPI = axios.create({
-    baseURL: '/api/v1',
+    baseURL: `${API_URL}/api/v1`,
     headers: {
         'Content-Type': 'application/json',
     }
